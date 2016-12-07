765771863fb1e39dde83066f8b1ae27f5382ed00
NatashaDianeey <ndianeey@gmail.com>
2016-12-07 12:32:09 -0600

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -7,10 +7,19 @@ $(document).ready( function(){
     /* Etapa 8 */
     $('.js-show-recipe').on('click', function() {
  		$('.page.recipe').removeClass('make');
+        
+        /*Etapa 9*/
+        $('.js-show-recipe').addClass('active');
+ 		$('.js-show-make').removeClass('active');  
  	});
  
  	$('.js-show-make').on('click', function() {
  		$('.page.recipe').addClass('make');
- 	});
-    
+ 	
+        /*Etapa 9*/
+        $('.js-show-make').addClass('active');
+        $('.js-show-recipe').removeClass('active');
+    });
 });
+
+
