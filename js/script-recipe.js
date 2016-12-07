a78ddf529580fcf27bbb5beeeead29b54ecc4ec1
NatashaDianeey <ndianeey@gmail.com>
2016-12-07 12:01:10 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,4 +3,14 @@ $(document).ready( function(){
 
     $('.js-menu').hide();
     console.log('Esta listo el documento');
+    
+    /* Etapa 8 */
+    $('.js-show-recipe').on('click', function() {
+ 		$('.page.recipe').removeClass('make');
+ 	});
+ 
+ 	$('.js-show-make').on('click', function() {
+ 		$('.page.recipe').addClass('make');
+ 	});
+    
 });
